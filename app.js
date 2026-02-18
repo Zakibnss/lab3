f856a3723cdd7e1dbc31f0a87a30b8ff8a5d93df
zakibnss <bnsszaki94@gmail.com>
2026-02-18 10:09:04 +0100

    V3: Added solar battery monitoring service

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 
 const express = require('express');
-const irrigationRoutes = require('./routes/irrigation');
+const sensorRoutes = require('./routes/sensor');
+const batteryRoutes = require('./routes/battery');
 
 const app = express();
 app.use(express.json());
 
-app.use('/irrigation', irrigationRoutes);
+app.use('/sensors', sensorRoutes);
+app.use('/battery', batteryRoutes);
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
-print("Server running on port 3000");
